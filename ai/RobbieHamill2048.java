01dbd924c4b696953779a55c2048f91336f03771
Robbie Hamill <ekb12139@uni.strath.ac.uk>
2015-05-05 22:34:52 +0100

    Retried using depth first algorithm

--- ai/RobbieHamill2048.java
+++ ai/RobbieHamill2048.java
@@ -1,9 +1,11 @@
 package ai;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.Random;
 import java.util.Stack;
 
@@ -16,79 +18,59 @@ public class RobbieHamill2048 extends AbstractPlayer {
 
 	private final Evaluator eval = new ScoreEvaluator();
 	private final Random random = new Random();
-	private Map<MOVE, Double> possibleMoves = new HashMap<MOVE, Double>();
 
 	@Override
 	public MOVE getMove(State game) {
-		int depth = 2;
+
 		pause();
+		
 		List<MOVE> bestMoves = new ArrayList<MOVE>();
+		List<MOVE> possibleMoves = new ArrayList<MOVE>();
+		
 		double bestScore = Double.NEGATIVE_INFINITY;
-		// double score = Double.POSITIVE_INFINITY;
-		for (MOVE move : game.getMoves()) {
+		
+		
+		for(MOVE move : game.getMoves()) {
 			game.move(move);
+			possibleMoves.add(move);
 			double score = eval.evaluate(game);
-			System.out.println(score);
-			game.undo();
-			score = alphabeta(game, depth, Double.NEGATIVE_INFINITY,Double.POSITIVE_INFINITY, true);
-			System.out.println(score);
-			if (score > bestScore) {
+			
+			for(int i = 0; i<7; i++){
+			for(MOVE move2 : game.getMoves()) {
+				game.move(move2); 
+				possibleMoves.add(move2);
+				score = eval.evaluate(game);
+		
+							
+				//break;
+						}
+			}
+			
+			for(int j = 0; j<possibleMoves.size(); j++){
+				game.undo();
+			}
+			
+			if(score > bestScore) {
 				bestMoves.clear();
 				bestMoves.add(move);
 				bestScore = score;
-				 } else if (score == bestScore) {
-				 bestMoves.add(move);
-				 }
+			} else if (score == bestScore) {
+				bestMoves.add(move);
 			}
-			// return bestMoves.get(random.nextInt(bestMoves.size()));
-			System.out.println(bestMoves);
+			
 		}
-		return bestMoves.get(0);
+		
+//		let Agenda = [S0
+//		              ]
+//		             while Agenda ≠ [] do
+//		             let Current = First(Agenda)
+//		             let Agenda = Rest(Agenda)
+//		             if Goal(Current) then return(“Found it!”)
+//		             let Next = NextStates(Current)
+//		             let Agenda = Next+Agenda
+//		
+		return bestMoves.get(random.nextInt(bestMoves.size()));
 	}
-
-	public double alphabeta(State game, int depth, double a, double b,
-			boolean studentName) {
-
-		// System.out.println("a" + a);
-		// System.out.println("b" + b);
-		if (depth == 0 || game.getMoves().isEmpty()) {
-			return a;
-		}
-		if (studentName) {
-			for (MOVE move : game.getMoves()) {
-				game.move(move);
-				a = eval.evaluate(game);
-				possibleMoves.put(move, a);
-				game.undo();
-				a = Math.max(a, alphabeta(game, depth - 1, a, b, false));
-				// game.move(move);
-				// a = eval.evaluate(game);
-				// game.undo();
-				if (b <= a) {
-					break;
-				}
-				return a;
-			}
-
-		} else {
-			for (MOVE move : game.getMoves()) {
-				game.move(move);
-				b = eval.evaluate(game);
-				possibleMoves.put(move, b);
-				game.undo();
-				b = Math.min(b, alphabeta(game, depth - 1, a, b, true));
-				// game.move(move);
-				b = eval.evaluate(game);
-				game.undo();
-				if (b <= a) {
-					break;
-				}
-				return b;
-			}
-		}
-		return b;
-	}
-
 	@Override
 	public int studentID() {
 		return 201213786;
